d8f10e38f7ebc319c23f5cd52f387ebc0e54bdf7
citzeye <citzeye@gmail.com>
2026-04-27 09:17:28 +0700

    update

--- src/core/library/library.rs
+++ src/core/library/library.rs
@@ -107,7 +107,7 @@ impl LibraryManager {
     pub fn add_folder(&mut self, path: String) {
         let name = Path::new(&path)
             .file_name()
-            .map(|n| n.to_string_lossy().to_string())
+            .map(|n| n.to_string_lossy().to_string().to_uppercase())
             .unwrap_or_else(|| path.clone());
         self.custom_folders.push((name.clone(), path.clone()));
         self.custom_folder_count = self.custom_folders.len() as i32;
